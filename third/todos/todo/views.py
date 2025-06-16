de4182332d7c2dab64e1bcc69b92a9cfeba054b2
V.Tsutsurin <v.tsutsurin@yandex.ru>
2025-06-16 03:32:33 +0300

    11.06.2025

--- third/todos/todo/views.py
+++ third/todos/todo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 def signupuser(request):
     if request.method == 'GET':
@@ -25,6 +28,15 @@ def signupuser(request):
                 'form': UserCreationForm(),
                 'error': 'Пароли не совпадают'
             })
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
