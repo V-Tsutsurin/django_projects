9ba592e46c690c9057cbb5c7083b1a3498aa4dc1
V.Tsutsurin <v.tsutsurin@yandex.ru>
2025-06-18 17:01:51 +0300

    16.06.2025

--- third/todos/todo/views.py
+++ third/todos/todo/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TodoForm
 
 
 def home(request):
     return render(request, 'todo/home.html')
+
 
 def signupuser(request):
     if request.method == 'GET':
@@ -29,14 +31,31 @@ def signupuser(request):
                 'error': 'Пароли не совпадают'
             })
 
+
 def loginuser(request):
     if request.method == "GET":
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {
+                'form': AuthenticationForm(),
+                'error': 'Неверные данные для входа'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
+
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
+
+
+def createtodo(request):
+    return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+
