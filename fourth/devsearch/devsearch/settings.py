6f8e1ac46d86d737e1f76906678c6717f62c5ce0
V.Tsutsurin <v.tsutsurin@yandex.ru>
2025-07-10 16:53:11 +0300

    10.07.2025

--- fourth/devsearch/devsearch/settings.py
+++ fourth/devsearch/devsearch/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +119,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
